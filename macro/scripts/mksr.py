e83be2a1301da9a7f8d1a943a49713eb81925c08
I-Huan CHIU <ichiu@chem.sci.osaka-u.ac.jp>
2020-08-27 11:49:24 +0900

    update weighting image for MLEM

--- macro/scripts/mksr.py
+++ macro/scripts/mksr.py
@@ -21,22 +21,32 @@ import enums
 from root_numpy import hist2array, array2hist, tree2array
 import numpy as np
 
-def GetImageSpace(filename,npixels,mypoint):
-    # Object spcae to image spcae
+def GetImageSpace(filename,npixels,npoints,nbins,mypoint):
+    # Object spcae to image spcae (by weighting image)
+    pixel_axis=[]
+    index,stepsize=0,10
+    for ix in range(npoints):
+       for iy in range(npoints):
+          for iz in range(npoints):
+             pixel_axis.append([(ix-2)*stepsize, (iy-2)*stepsize, (iz-2)*stepsize])
+
     weight, sumw = 0,0
     imagearray_list, weight_list=[],[]
+    myimageA = np.zeros((nbins,nbins),dtype=float)
     f=ROOT.TFile(filename,"read")
-    for i in range(n_images):
+    for i in range(pow(npoints,3)):
+      # get cal. images
       _name = "image_pos"+str(i)
       imagearray_list.append(hist2array(f.Get(_name)))
-    for i in range(len(imagearray_list)):
-       # TODO make mypoint and pixel_axis
-       # mypoint, and pixel_axis[i] are array [x,y,z]
-       weight=np.sum((mypoint-pixel_axis[i])**2)#Inverse square distance
+    for i in range(pow(npoints,3)):
+       # get weight from each imges
+       if (np.sum(((mypoint-pixel_axis[i])**2)) == 0): mypoint=mypoint+0.00000000001 #skip divide by zero
+       weight=1/np.sum(((mypoint-pixel_axis[i])**2))#Inverse square distance
        sumw+=weight
        weight_list.append(weight)# use this weight_list to find image for mypoint
-    for i in range(len(imagearray_list)):
-       myimageA+=imagearray_list[i]*weight_list[i]
+    for i in range(pow(npoints,3)):
+       # find corresponding image from the giving object space
+       myimageA+=imagearray_list[i]*(weight_list[i]/sumw)
     return myimageA
 
 def mkSystemResponse(filename,n_images):
@@ -48,26 +58,24 @@ def mkSystemResponse(filename,n_images):
        _name = "image_pos"+str(i)
        image_list.append(f.Get(_name))
     _sr=np.zeros((len(image_list),image_list[0].GetNbinsX(),image_list[0].GetNbinsY()),dtype=float)
-    print(image_list[0].GetXaxis().GetBinCenter(1))
     for i in range(len(image_list)):
        _sr[i]=hist2array(image_list[i])
     _sr = _sr/(am241_intensity*_time)
-    print(_sr[0])
     return _sr
 
-def GuessInitObjectSpace(_object):
-    # make inital object 
-    return _obforSR
-
-#def getImageSpace(_object):
-#    _image = _object*A # ???
-#    return _image
+def GuessInitObjectSpace(_object,npixels):
+    _obAxis=[]
+    for _index in range(pow(npixels,3)):
+       _obAxis=[10,10,10]#make axis from object space
+    return _obAxis
 
 def IterativeMLEM(_sysRespond, _object, _image):
     return 0
 
 def testrun(args):
     _sr=mkSystemResponse(args.inputFolder, args.nimages)
+    testpoint=np.array([5,5,5])
+    testimage = GetImageSpace(args.inputFolder,128,5,128,testpoint)
 
     #no need
     fout=ROOT.TFile("/Users/chiu.i-huan/Desktop/mytesth3output.root", 'recreate' )
@@ -75,6 +83,10 @@ def testrun(args):
     array2hist(_sr,h3)
     fout.cd()
     h3.Write()
+
+    h2=ROOT.TH2D("h2","h2",testimage.shape[0], -16, 16,testimage.shape[1], -16, 16)
+    array2hist(testimage,h2)
+    h2.Write()
 
     exit(0)
 
