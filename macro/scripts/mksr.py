5f89ac8c4876be0ee4611b973f719f994281db04
I-Huan CHIU <ichiu@chem.sci.osaka-u.ac.jp>
2020-09-03 18:07:52 +0900

    add comparison plots (still need to debug)

--- macro/scripts/mksr.py
+++ macro/scripts/mksr.py
@@ -32,6 +32,7 @@ gROOT.ProcessLine(
 
 from ROOT import MLEMStruct
 mlstruct = MLEMStruct()
+paramater_list,point_axis=[],[]
 
 # ======================= find paramater for fitting ===================
 class PrepareParameters():
@@ -41,26 +42,29 @@ class PrepareParameters():
           self.stepsize=stepsize
           self.npixels=npixels
           self.nbins=nbins
-          self.imagearray, self.XYZaxis = self.getimages()
+          self.imagearray = self.getimages()
+          self.par_list = self.fitting_para()
 
       def getimages(self):
-          imagearray, XYZaxis=[], []
+          global point_axis
+          imagearray,point_axis=[],[]
           f=ROOT.TFile(self.filename,"read")
           for ix in range(self.npoints):
              for iy in range(self.npoints):
                 for iz in range(self.npoints):
                    index=ix+iy*5+iz*25
-                   XYZaxis.append([(ix-2)*self.stepsize, (iy-2)*self.stepsize, (iz-2)*self.stepsize])
+                   point_axis.append([(ix-2)*self.stepsize, (iy-2)*self.stepsize, (iz-2)*self.stepsize])
                    _name = "image_pos"+str(index)
                    imagearray.append(hist2array(f.Get(_name)))
-          return imagearray, XYZaxis
+          return imagearray
 
       def getfunc(self, name, _down, _up):
           myfunction = "gaus"
           return ROOT.TF1(name,myfunction,_down,_up)
 
       def fitting_para(self):
-          _paramater_list=[]
+          global paramater_list
+          paramater_list=[]
           fit_range,fit_step=3,7.5
           index, xup, xdown, yup, ydown=0,0,0,0,0
           for ix in range(self.npoints):
@@ -81,13 +85,9 @@ class PrepareParameters():
                    _hy.Fit("gy","QR")                 
                    mean_x, mean_y, sigma_x, sigma_y = gx.GetParameter(1), gy.GetParameter(1), gx.GetParameter(2), gy.GetParameter(2)
                    intensity= _hx.GetEntries()
-                   _paramater_list.append([mean_x, mean_y, sigma_x, sigma_y, intensity])                   
+                   paramater_list.append([mean_x, mean_y, sigma_x, sigma_y, intensity])                   
                    index+=1
-          return _paramater_list
-
-PP=PrepareParameters(filename="/Users/chiu.i-huan/Desktop/new_scientific/run/root/20200406a_5to27_cali_caldatat_0828_split.root",npoints=5,stepsize=10,npixels=125,nbins=128)
-paramater_list=PP.fitting_para()
-pixel_axis=PP.XYZaxis
+          return paramater_list
 
 # ======================= varaible fitting for function ===================
 def deffunc(_x,_y,_z,par):         
@@ -96,27 +96,27 @@ def deffunc(_x,_y,_z,par):
 def fcn_x(npar, gin, f, par, iflag):
     chisq, nbins = 0., 5
     for _index in range(pow(nbins,3)):
-       chisq += pow((paramater_list[_index][0] - deffunc(pixel_axis[_index][0],pixel_axis[_index][1],pixel_axis[_index][2],par)),2)
+       chisq += pow((paramater_list[_index][0] - deffunc(point_axis[_index][0],point_axis[_index][1],point_axis[_index][2],par)),2)
     f[0] = chisq
 def fcn_y(npar, gin, f, par, iflag):
     chisq, nbins = 0., 5
     for _index in range(pow(nbins,3)):
-       chisq += pow((paramater_list[_index][1] - deffunc(pixel_axis[_index][0],pixel_axis[_index][1],pixel_axis[_index][2],par)),2)
+       chisq += pow((paramater_list[_index][1] - deffunc(point_axis[_index][0],point_axis[_index][1],point_axis[_index][2],par)),2)
     f[0] = chisq
 def fcn_xsig(npar, gin, f, par, iflag):
     chisq, nbins = 0., 5
     for _index in range(pow(nbins,3)):
-       chisq += pow((paramater_list[_index][2] - deffunc(pixel_axis[_index][0],pixel_axis[_index][1],pixel_axis[_index][2],par)),2)
+       chisq += pow((paramater_list[_index][2] - deffunc(point_axis[_index][0],point_axis[_index][1],point_axis[_index][2],par)),2)
     f[0] = chisq
 def fcn_ysig(npar, gin, f, par, iflag):
     chisq, nbins = 0., 5
     for _index in range(pow(nbins,3)):
-       chisq += pow((paramater_list[_index][3] - deffunc(pixel_axis[_index][0],pixel_axis[_index][1],pixel_axis[_index][2],par)),2)
+       chisq += pow((paramater_list[_index][3] - deffunc(point_axis[_index][0],point_axis[_index][1],point_axis[_index][2],par)),2)
     f[0] = chisq
 def fcn_inten(npar, gin, f, par, iflag):
     chisq, nbins = 0., 5
     for _index in range(pow(nbins,3)):
-       chisq += pow((paramater_list[_index][4] - deffunc(pixel_axis[_index][0],pixel_axis[_index][1],pixel_axis[_index][2],par)),2)
+       chisq += pow((paramater_list[_index][4] - deffunc(point_axis[_index][0],point_axis[_index][1],point_axis[_index][2],par)),2)
     f[0] = chisq
 
 class SystemResponse():
@@ -175,11 +175,11 @@ class SystemResponse():
 
 # ======================= Maximum Likelihood Expectation Maximization ===================
 class MLEM():
-      def __init__(self,para_dic=None,nbins=None):
+      def __init__(self,para_dic=None,nbins=None,ori_image_list=None):
           self.nbins=nbins
           self.para_dic=para_dic
-          self.image_var=self.srf(0,0,0)
-          self.mlemh2,self.mlemhx,self.mlemhy=self.mkimage()
+          self.ori_image_list=ori_image_list
+          self.image_hx_hy_list_ori, self.image_hx_hy_list_sr=self.mkimage()
           self.mlemtree=self.mktree()
 
       def srf(self,_x,_y,_z):
@@ -199,38 +199,67 @@ class MLEM():
           _tree.SetDirectory(0)
           _tree.Branch( 'mlemx', AddressOf( mlstruct, 'mlemx' ),  'mlemx/D' )
           _tree.Branch( 'mlemy', AddressOf( mlstruct, 'mlemy' ),  'mlemy/D' )
-          for ie in range(int(self.image_var[4])):
+          for ie in range(int(100)):
              _tree.Fill()
           return _tree
 
       def mkimage(self):
-          hx=ROOT.TH1D("hx","hx",self.nbins,-16,16)
-          hy=ROOT.TH1D("hy","hy",self.nbins,-16,16)
-          h2=ROOT.TH2D("image","image",self.nbins,-16,16,self.nbins,-16,16)
-          hx_gaus = ROOT.TF1("hx_gaus","TMath::Gaus(x,{0},{1})".format(self.image_var[0],self.image_var[2]),-16,16)
-          hy_gaus = ROOT.TF1("hy_gaus","TMath::Gaus(x,{0},{1})".format(self.image_var[1],self.image_var[3]),-16,16)
-          hx.FillRandom("hx_gaus",int(self.image_var[4]))
-          hy.FillRandom("hy_gaus",int(self.image_var[4]))
-          for ix in range(128):
-             for iy in range(128):
-                xvalue=hx.GetBinContent(ix+1)
-                yvalue=hy.GetBinContent(iy+1)
-                _bin=h2.GetBin(ix,iy)
-                if(xvalue!=0 and yvalue!=0): h2.SetBinContent(_bin,(xvalue+yvalue)/2)
-          return h2,hx,hy
+          # make original vs. system response comparison plots
+          image_hx_hy_list_sr, image_hx_hy_list_ori=[],[]
+          for _ip in range(len(point_axis)):
+             # original plots
+             _h2name="image_ori_"+str(_ip)             
+             _h2=ROOT.TH2F(_h2name,_h2name,self.nbins,-16,16,self.nbins,-16,16) 
+             array2hist(self.ori_image_list[_ip],_h2)
+             image_hx_hy_list_ori.append(_h2)
+             image_hx_hy_list_ori.append(_h2.ProjectionX())
+             image_hx_hy_list_ori.append(_h2.ProjectionY())
+
+             # system response plots
+             image_var = self.srf(point_axis[_ip][0], point_axis[_ip][1], point_axis[_ip][2])
+             _h2name="image_sr_"+str(_ip)
+             h2=ROOT.TH2D(_h2name,_h2name,self.nbins,-16,16,self.nbins,-16,16)
+             hx=ROOT.TH1D(_h2name+"hx",_h2name+"hx",self.nbins,-16,16)
+             hy=ROOT.TH1D(_h2name+"hy",_h2name+"hy",self.nbins,-16,16)
+             hx_gaus = ROOT.TF1("hx_gaus","TMath::Gaus(x,{0},{1})".format(image_var[0],image_var[2]),-16,16)
+             hy_gaus = ROOT.TF1("hy_gaus","TMath::Gaus(x,{0},{1})".format(image_var[1],image_var[3]),-16,16)
+             hx.FillRandom("hx_gaus",int(image_var[4]))
+             hy.FillRandom("hy_gaus",int(image_var[4]))
+             for ix in range(self.nbins):
+                for iy in range(self.nbins):
+                   xvalue=hx.GetBinContent(ix+1)
+                   yvalue=hy.GetBinContent(iy+1)
+                   _bin=h2.GetBin(ix,iy)
+                   if(xvalue!=0 and yvalue!=0): h2.SetBinContent(_bin,(xvalue+yvalue)/2)
+             image_hx_hy_list_sr.append(h2)
+             image_hx_hy_list_sr.append(hx)
+             image_hx_hy_list_sr.append(hy)             
+          return image_hx_hy_list_ori, image_hx_hy_list_sr
 
       def iterate(self):
           return 0
 
+      def printoutput(self,RootType_list, _outname, savetype):
+          if savetype == "re" or savetype == "RE" or savetype == "Re": _savetype = "recreate"
+          if savetype == "up" or savetype == "UP" or savetype == "Up": _savetype = "update"
+          fout=ROOT.TFile(_outname, _savetype)
+          fout.cd()    
+          if isinstance(RootType_list, list):
+             for _iR in RootType_list:
+                _iR.Write()
+          else:
+             RootType_list.Write()
+          fout.Close()
+
 # ======================= test part ===================
 def GetImageSpace(filename,npixels,npoints,nbins,mypoint):
     # Object spcae to image spcae (by weighting image)
-    mypixel_axis=[]
+    my_axis=[]
     index,stepsize=0,10
     for ix in range(npoints):
        for iy in range(npoints):
           for iz in range(npoints):
-             mypixel_axis.append([(ix-2)*stepsize, (iy-2)*stepsize, (iz-2)*stepsize])
+             my_axis.append([(ix-2)*stepsize, (iy-2)*stepsize, (iz-2)*stepsize])
 
     weight, sumw = 0,0
     imagearray_list, weight_list=[],[]
@@ -242,8 +271,8 @@ def GetImageSpace(filename,npixels,npoints,nbins,mypoint):
       imagearray_list.append(hist2array(f.Get(_name)))
     for i in range(pow(npoints,3)):
        # get weight from each imges
-       if (np.sum(((mypoint-mypixel_axis[i])**2)) == 0): mypoint=mypoint+0.00000000001 #skip divide by zero
-       weight=1/np.sum(((mypoint-mypixel_axis[i])**2))#Inverse square distance
+       if (np.sum(((mypoint-my_axis[i])**2)) == 0): mypoint=mypoint+0.00000000001 #skip divide by zero
+       weight=1/np.sum(((mypoint-my_axis[i])**2))#Inverse square distance
        sumw+=weight
        weight_list.append(weight)# use this weight_list to find image for mypoint
     for i in range(pow(npoints,3)):
@@ -251,7 +280,7 @@ def GetImageSpace(filename,npixels,npoints,nbins,mypoint):
        myimageA+=imagearray_list[i]*(weight_list[i]/sumw)
     return myimageA
 
-def mkSystemResponse(filename,_np):
+def mkWeightFunc(filename,_np):
     n_images=pow(_np,3)
     am241_intensity = 363.1*1000 #Bq
     _time=600 #second
@@ -268,37 +297,32 @@ def mkSystemResponse(filename,_np):
 
 # ======================= run ===================
 def testrun(args):
+    outfilename = "/Users/chiu.i-huan/Desktop/mytesth3output.root"
     log().info("Test run...")
-    _sr=mkSystemResponse(args.inputFolder, args.npoints)
+    _sr=mkWeightFunc(args.inputFolder, args.npoints)
     testpoint=np.array([0,0,0])
     testimage = GetImageSpace(args.inputFolder,128,5,128,testpoint)
 
-    log().info("System response...")
-    SR=SystemResponse()# fitting and print result
-    ML=MLEM(para_dic=SR.par_x_y_xs_ys_inten,nbins=128)
+    log().info("Progressing System response and making MLEM plots...")
+    PP=PrepareParameters(filename=args.inputFolder,npoints=args.npoints,stepsize=args.stepsize,npixels=args.npixels,nbins=128) # get cali. image list
+    SR=SystemResponse()# get system response by TMinuit fitting
+    ML=MLEM(para_dic=SR.par_x_y_xs_ys_inten,nbins=128,ori_image_list=PP.imagearray) # do iterate and get final plots
 
     #no need
-    fout=ROOT.TFile("/Users/chiu.i-huan/Desktop/mytesth3output.root", 'recreate' )
     h3=ROOT.TH3D("h3","h3",_sr.shape[0], 0, 125,_sr.shape[1], -16, 16,_sr.shape[2],-16,16)
     array2hist(_sr,h3)
-    fout.cd()
-#    h3.Write()
+#    ML.printoutput(h3,outfilename,"re")
 
-    log().info("Making MLEM plots...")
-    ML.mlemtree.Write()
-    ML.mlemhx.Write()
-    ML.mlemhy.Write()
-    ML.mlemh2.Write()
+    log().info("Print outputs...")
+    ML.printoutput(ML.mlemtree,outfilename,"re")
+    ML.printoutput(ML.image_hx_hy_list_ori,outfilename,"up")
+    ML.printoutput(ML.image_hx_hy_list_sr,outfilename,"up")
 
     h2=ROOT.TH2D("h2","h2",testimage.shape[0], -16, 16,testimage.shape[1], -16, 16)
     array2hist(testimage,h2)
-    _h1x=h2.ProjectionX()
-    _h1y=h2.ProjectionY()
-    _h1x.Write()
-    _h1y.Write()
-    h2.Write()
+#    ML.printoutput([h2.ProjectionX(),h2.ProjectionY(),h2],outfilename,"up")
 
-    log().info("Output : %s"%("/Users/chiu.i-huan/Desktop/mytesth3output.root"))
+    log().info("Output : %s"%(outfilename))
     exit(0)
 
 if __name__=="__main__":
@@ -306,6 +330,7 @@ if __name__=="__main__":
    parser = argparse.ArgumentParser(description='Process some integers.')
    parser.add_argument("-i","--inputFolder", type=str, default="/Users/chiu.i-huan/Desktop/new_scientific/run/root/20200406a_5to27_cali_caldatat_0828_split.root", help="Input File Name")
    parser.add_argument("-n","--npoints",dest="npoints",type=int, default=5, help="Number of images")
+   parser.add_argument("-s","--stepsize",dest="stepsize",type=int, default=10, help="Number of images")
    parser.add_argument("-p","--npixels",dest="npixels",type=int, default=50, help="Number of images")
    args = parser.parse_args()
 
