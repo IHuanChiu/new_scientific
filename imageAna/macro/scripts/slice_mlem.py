737cf1a5557935689d47e0c1e7af2fcfdd7f9863
I-Huan CHIU <ichiu@chem.sci.osaka-u.ac.jp>
2020-11-25 13:37:37 +0900

    change color

--- imageAna/macro/scripts/slice_mlem.py
+++ imageAna/macro/scripts/slice_mlem.py
@@ -1,7 +1,7 @@
 import sys,os,ROOT,time
 sys.path.append('/Users/chiu.i-huan/Desktop/new_scientific/imageAna/macro/utils/')
 sys.path.append('/Users/chiu.i-huan/Desktop/new_scientific/imageAna/macro/')
-from ROOT import gSystem, gPad, gDirectory
+from ROOT import gSystem, gPad, gDirectory, gStyle
 from helpers import createRatioCanvas, ProgressBar
 from logger import log
 from root_numpy import hist2array, array2hist, tree2array
@@ -10,10 +10,12 @@ ROOT.gErrorIgnoreLevel = ROOT.kWarning
 __location__ = os.path.realpath(
         os.path.join(os.getcwd(), os.path.dirname(__file__)))
 ROOT.gROOT.LoadMacro( __location__+'/AtlasStyle/AtlasStyle.C')
-ROOT.SetAtlasStyle()
+#ROOT.SetAtlasStyle()
 
 nplots=40
-inputname_list=["/Users/chiu.i-huan/Desktop/new_scientific/imageAna/run/root/MLEM_output/myMLEMoutput_1116_mlem_iteration100.root"]
+_outname="mlem_image"
+inputname_list=["/Users/chiu.i-huan/Desktop/new_scientific/imageAna/run/root/MLEM_output/myMLEMoutput_1111_mlemall_iteration30.root"]
+#inputname_list=["/Users/chiu.i-huan/Desktop/new_scientific/imageAna/run/root/MLEM_output/myMLEMoutput_1109_cut0p01_iteration15.root"]
 
 def namelist(_name):
     if _name == "x": return ["Y [mm]", "Z [mm]"]
@@ -39,12 +41,18 @@ def doslice(hist3d,outname,axisname):
        _h3temp = hist3d.Clone()
        _u, _d = (20 - (40./nplots)*i), (20 - (40./nplots)*(i+1))
        setrange(_h3temp, axisname, _u, _d)
-       _h2 = _h3temp.Project3D(scan_axis)
+       _h2temp = _h3temp.Project3D(scan_axis)
+       _h2=_h2temp.Clone()
+       _array=hist2array(_h2temp)
+       _array[np.where(_array == 0)]=0.001
+       array2hist(_array,_h2)
        _h2.SetStats(0)
        _h2.SetTitle("slice %s %.1f mm"%(axisname,(_d+_u)/2.)) 
        _h2.GetXaxis().SetTitle(titlename[0])
        _h2.GetYaxis().SetTitle(titlename[1])
+       _h2.GetZaxis().SetRangeUser(0, 100)
        gPad.SetLogz(0)
+       gStyle.SetPalette(62)
        _h2.Draw("colz")
        cv.Print(name, "pdf") 
     cv.Print(name + "]", "pdf");
@@ -52,8 +60,8 @@ def doslice(hist3d,outname,axisname):
 for _if in inputname_list:
    f_mlem=ROOT.TFile(_if,"read")
    h3=f_mlem.Get("MLEM_3Dimage")
-   doslice(h3,"test","x")
-   doslice(h3,"test","y")
-   doslice(h3,"test","z")
+   doslice(h3,_outname,"x")
+   doslice(h3,_outname,"y")
+   doslice(h3,_outname,"z")
       
 log().info("Path of pdf: {}".format("/Users/chiu.i-huan/Desktop/new_scientific/imageAna/run/figs/3Dslices/"))
