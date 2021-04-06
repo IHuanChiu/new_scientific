263765f8d0d4973b57aa8880f28bceed597f0634
I-Huan CHIU <ichiu@chem.sci.osaka-u.ac.jp>
2021-04-06 20:53:57 +0900

    add CdTe123 scripts

--- imageAna/run/fullrun.sh
+++ imageAna/run/fullrun.sh
@@ -11,7 +11,9 @@ OUTNAME="test1020"
 #fi
 
      
+# =====
 # ================== Simple test run ====================
+# =====
 python3 main.py ${NTUPDIRCDTE}/20200307a_00072_001.root -o oldtest_20201005
 #jparc1205 test
 #python3 main.py /Users/chiu.i-huan/Desktop/new_scientific/imageAna/data/JPARC2020Dec/merge_root/sample_particle.root -d CdTe_JPARCDec -e ${MACRODIR}/auxfile/cdtedsd_2020a_cal_3_m5c400v.root -o 201215_2
@@ -26,8 +28,9 @@ python3 main.py ${NTUPDIRCDTE}/20200307a_00072_001.root -o oldtest_20201005
 
 
 
-
+# =====
 # ================== 2mm CdTe run ====================
+# =====
  # *** battery image sample ***
 #parallel --eta -j ${NCORES} python3 main.py /Users/chiu.i-huan/Desktop/new_scientific/imageAna/data/battery_data/{1}/cdtedsd_2020b_0917a_000{2}_001.root -d CdTe_Lab -o battery_{1} -e ${MACRODIR}/auxfile/spline_calibration_2mmtest_merge_1008.root ::: Ba ::: 05 06 07 08 09 10 11 12 13 14 15 16 17
 #parallel --eta -j ${NCORES} python3 main.py /Users/chiu.i-huan/Desktop/new_scientific/imageAna/data/battery_data/{1}/cdtedsd_2020b_0917a_000{2}_001.root -d CdTe_Lab -o battery_{1} -e ${MACRODIR}/auxfile/spline_calibration_2mmtest_merge_1008.root ::: Co ::: 19 20 21 22 23
@@ -46,7 +49,10 @@ python3 main.py ${NTUPDIRCDTE}/20200307a_00072_001.root -o oldtest_20201005
 #parallel --eta -j ${NCORES} python3 main.py ../data/minami_data/500n20/{1}/cdtedsd_2020b_0720a_000{2}_001.root -d CdTe_Lab -o 500n20_{1}source_cali_{3} -e ${MACRODIR}/auxfile/spline_calibration_2mmtest_{3}.root -n 2000000 ::: Co ::: 14 15 16 17 18 ::: merge_1008
 
 
+
+# =====
 # ================== make ntuplke & plots J-PARC 2020March ====================
+# =====
 #parallel --eta -j ${NCORES} python3 main.py ${NTUPDIRCDTE}_merge/20200307a_{1}.root -o merge ::: 55to73 77to95 95to111
    # *** Si ***
 #parallel --eta -j ${NCORES} python3 main.py ${NTUPDIRSI}/test_f_000{1}_001.root -d Si -e ${MACRODIR}/auxfile/spline_calibration.root ::: 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 
@@ -54,11 +60,20 @@ python3 main.py ${NTUPDIRCDTE}/20200307a_00072_001.root -o oldtest_20201005
 #parallel --eta -j ${NCORES} python3 main.py ${NTUPDIRCDTE}/20200307a_00057_001.root -o cut{1}_delta{2} -cut {1} -m {2} ::: 5 10 20 40 ::: 10 5 3 2
 #parallel --eta -j ${NCORES} python3 main.py ${NTUPDIRSI}/test_f_00007_001.root -d Si -e ${MACRODIR}/auxfile/spline_calibration.root -o cut{1}_delta{2} -cut {1} -m {2} ::: 2 3 8 12 ::: 10 5 3 2
 
+
+
+# =====
 # ================== run rotation 2D plots and FBP image ====================
+# =====
 #python3 image.py -i ${OUTDIR}/CdTe_root_fix -d CdTe -o LP_0909 
 #python3 image.py -i ${OUTDIR}/CdTe_root_fix -d CdTe -o 35MeV
 #python3 image.py -i ${OUTDIR}/Si_root_fix -d Si -o 1120
 
+
+
+
+# =====
 # ================== run MLEM ====================
+# =====
 #python3 mksr.py --imageinput /Users/chiu.i-huan/Desktop/new_scientific/imageAna/run/figs/repro_3Dimage.CdTe_LP_0909.root 
 python3 mksr.py --imageinput /Users/chiu.i-huan/Desktop/new_scientific/imageAna/run/figs/repro_3Dimage.CdTe_30MeV.root -o 30MeV -l 1
